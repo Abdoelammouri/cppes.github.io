db9db088d6da773d6af500c04095e866e8029f61
fabridegor <33553258+fabridegor@users.noreply.github.com>
2017-11-13 23:44:21 +0100

    Update Calendario.cpp

--- Esercizi C++/Calendario.cpp
+++ Esercizi C++/Calendario.cpp
@@ -1,8 +1,8 @@
 /*
 Acquisire un valore intero >1582 e dire se corrisponde a un anno bisestile oppure no
-Un anno é bisestile se divisibile per 400( se centenario) o se sono divisibili per 4 (no centenario)
-2000 é anno bisestile
-1900 non é bisestile
+Un anno Ã© bisestile se divisibile per 400( se centenario) o se sono divisibili per 4 (no centenario)
+2000 Ã© anno bisestile
+1900 non Ã© bisestile
 */
 #include <iostream>
 using namespace std;
@@ -12,9 +12,11 @@ int main(){
     cout << "Scrivi un anno" << endl;
     cin >> anno;
 
-    if(anno<1582)
+    if(anno<1582) //controllo validitÃ¡ anno
         cerr << "Anno non valido" << endl;
     else{
+        /* controllo se anno Ã© centenario e bisestile, se si lo stampo, altrimenti controllo che non sia centenario
+           e che sia bisestile */
         if(anno%100==0 && anno%400==0)
             cout<< anno << " centenario bisestile" << endl;
         else{

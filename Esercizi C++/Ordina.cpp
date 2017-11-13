e8b3cb97976241125e38d848868b9c5382d5f61c
fabridegor <33553258+fabridegor@users.noreply.github.com>
2017-11-13 23:33:37 +0100

    Update Ordina.cpp

--- Esercizi C++/Ordina.cpp
+++ Esercizi C++/Ordina.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    int valore, valore1, valore2, valore3;
+    int valoreApp, valore1, valore2, valore3;
     cout << "Dammi tre valori" << endl;
     cin >> valore1;
     cin >> valore2;
@@ -11,23 +11,23 @@ int main(){
 
 
 
-
-    if (valore1>valore2){
-        valore=valore2;
+// controllo il primo valore immesso con il secondo e con il terzo in modo da controllare se sia il piú piccolo 
+    if (valore1>valore2){   
+        valoreApp=valore2; //valoreApp é la variabile di appoggio che mi serve per non perdere il dato di valore2
         valore2=valore1;
-        valore1=valore;
+        valore1=valoreApp;
     }
 
     if(valore1>valore3){
-        valore=valore3;
+        valoreApp=valore3;
         valore3=valore1;
-        valore1=valore;
+        valore1=valoreApp;
     }
 
     if (valore2>valore3){
-            valore=valore3;
+            valoreApp=valore3;
             valore3=valore2;
-            valore2=valore;
+            valore2=valoreApp;
     }
     cout << valore1 << " " << valore2 << " "<< valore3 << endl;
 
